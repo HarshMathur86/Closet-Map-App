26d1e9fb554bf397df74f54e5a670442354dec40
harshmathur86 <99mharsh@gmail.com>
2026-02-22 19:06:34 +0530

    Expo Build Issue Resolved

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -26,7 +26,7 @@ if (!admin.apps.length) {
 
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
-        projectId: process.env.FIREBASE_PROJECT_ID
+        projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID
     });
 }
 
